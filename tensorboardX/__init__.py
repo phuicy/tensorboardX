5a31f50d08df2017caa08649cd0d21f58a761e12
Julian <jeisenschlos@asapp.com>
2019-04-26 11:32:21 -0300

    Adding HParam Support

--- tensorboardX/__init__.py
+++ tensorboardX/__init__.py
@@ -5,4 +5,4 @@ from .record_writer import RecordWriter
 from .torchvis import TorchVis
 from .writer import FileWriter, SummaryWriter
 
-__version__ = "1.6"  # will be overwritten if run setup.py
+__version__ = "1.6+0bf6c07"  # will be overwritten if run setup.py
